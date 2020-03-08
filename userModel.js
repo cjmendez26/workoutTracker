4a2ea79966cd5fce0af4079007c506d332a4396f
Jane <cjmendez26@gmail.com>
2020-03-08 07:43:50 -0700

    changing

--- userModel.js
+++ userModel.js
@@ -1,33 +1,33 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: "Username is Required"
-  },
+// const UserSchema = new Schema({
+//   username: {
+//     type: String,
+//     trim: true,
+//     required: "Username is Required"
+//   },
 
-  password: {
-    type: String,
-    trim: true,
-    required: "Password is Required",
-    validate: [({ length }) => length >= 6, "Password should be longer."]
-  },
+//   password: {
+//     type: String,
+//     trim: true,
+//     required: "Password is Required",
+//     validate: [({ length }) => length >= 6, "Password should be longer."]
+//   },
 
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-  },
+//   email: {
+//     type: String,
+//     unique: true,
+//     match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+//   },
 
-  userCreated: {
-    type: Date,
-    default: Date.now
-  }
-});
+//   userCreated: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
 
-const User = mongoose.model("User", UserSchema);
+// const User = mongoose.model("User", UserSchema);
 
-module.exports = User;
+// module.exports = User;
